f693f9f4d2f976617f885ca98a4c5d6a7b1bad3f
Klosemoller <alan.correa.moeller@gmail.com>
2022-12-01 19:15:07 -0600

    Prueba test convertidor

---
+++ Compilador/Compilador/IntentoCodigoIntermedio/Convertidor string a txt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compilador.IntentoCodigoIntermedio
+{
+    internal class Convertidor_string_a_txt
+    {
+        public static bool serealizo;
+        public static void Convertidor(string CodigoIntermedio)
+        {
+            string path = "";
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(CodigoIntermedio);
+                    
+                }
+            }
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    MessageBox.Show(s);
+                    serealizo= true;
+                }
+            }
+        }
+    }
+}
---
+++ Compilador/TestProject1/Test_Convertidor/PruebaConvertidor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Compilador;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TestProject1.Test_Convertidor
+{
+    /*
+    internal class PruebaConvertidor
+    {
+        [Fact]
+        public static void Prueba_Datos()
+        {
+            string archivo = string.Format($"hola\n" +
+                $"adios\n" +
+                $"xd\n" +
+                $"shoot\n" +
+                $"alien\n" +
+                $"323\n");
+            Assert.True(Convertidor_string_a_txt);
+        }
+    }
+    */
+}
